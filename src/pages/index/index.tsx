1dc08365e1eae6bc016b17e110eee91017b73a0c
YAN7 <904394373@qq.com>
2021-11-04 18:05:33 +0800

    test

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,8 @@
 import { Component } from 'react'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Button } from '@tarojs/components'
 import { observer, inject } from 'mobx-react'
 
+import Taro from '@tarojs/taro'
 import './index.less'
 
 type PageStateProps = {
@@ -22,39 +23,67 @@ interface Index {
 @inject('store')
 @observer
 class Index extends Component {
-  componentWillMount () { }
+	state = {
+		rewordText: ''
+	}
 
-  componentDidMount () { }
+	componentDidMount() {
+		this.renderReword()
+	}
+	
+	clockIn = () => {
+		Taro.request({
+			url: 'http://localhost:3000/clock', //仅为示例，并非真实的接口地址
+			method: 'POST',
+			success: function (res) {
+				console.log(res)
+			}
+		})
+	}
 
-  componentWillUnmount () { }
+	renderReword = () => {
+		let text = ` \n `
+		setInterval(() => {
+			const currTime = new Date()
+			const currYear = currTime.getFullYear()
+			const currMonth = currTime.getMonth() + 1
+			const currDay = currTime.getDate()
+			const five = new Date(`${currYear}-${currMonth}-${currDay} 8:30:00`)
+			const difference = currTime.getTime() - five.getTime()
+			const hour = Math.floor((difference / 1000) / 3600)
+			const minute = Math.floor((difference / 1000 / 60) % 60)
+			const second = Math.floor((difference / 1000) % 60)
+			const rewardPerSecond = 500 / 8 / 3600
+			const rewardTotal = (rewardPerSecond * difference / 1000).toFixed(2)
+			text = `你已加班:${hour}小时${minute}分${second}秒, \n 已赚取${rewardTotal}元报酬`
+			this.setState({
+				rewordText: text
+			})
+		}, 1000)
+		return text
+	};
 
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  increment = () => {
-    const { counterStore } = this.props.store
-    counterStore.increment()
-  }
-
-  decrement = () => {
-    const { counterStore } = this.props.store
-    counterStore.decrement()
-  }
-
-  incrementAsync = () => {
-    const { counterStore } = this.props.store
-    counterStore.incrementAsync()
-  }
+	login = async () => {
+		const { code } = await Taro.login()
+		Taro.request({
+			url: 'http://localhost:3000/user/login',
+			method: 'POST',
+			data: {
+				code,
+			},
+			success: function (res) {
+				console.log(res)
+				Taro.getUserInfo()
+			}
+		})
+	}
 
   render () {
-    const { counterStore: { counter } } = this.props.store
     return (
       <View className='index'>
-        <Button onClick={this.increment}>+</Button>
-        <Button onClick={this.decrement}>-</Button>
-        <Button onClick={this.incrementAsync}>Add Async</Button>
-        <Text>{counter}</Text>
+				{/* <Text className="rewordText">{this.state.rewordText}</Text> */}
+        {/* <View className="greenStatus" onClick={this.clockIn}>立即打卡</View> */}
+				<Button openType="getAuthorize" onClick={this.login}>登陆</Button>
       </View>
     )
   }
